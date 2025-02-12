27be4dc09a9bdfd0e56a53308db6e2312cde0f4a
andrewWong21 <awong541@gmail.com>
2025-02-11 23:57:43 -0500

    Update register and login pages

--- grammotype/auth.py
+++ grammotype/auth.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        if len(email) == 0:
+            flash("Email cannot be empty", category="error")
+        elif len(firstName) == 0:
+            flash("Name cannot be empty", category="error")
+        elif len(password1) == 0:
+            flash("Password cannot be empty", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match", category="error")
+        else:
+            flash("Account successfully created.", category="success")
+
     return render_template("register.html")
