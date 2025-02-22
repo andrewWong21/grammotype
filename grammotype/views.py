01fb6369820f3fe68f2885392a52b632b1368b15
andrewWong21 <awong541@gmail.com>
2025-02-21 23:55:46 -0500

    Started homepage modification to feature typing test

--- grammotype/views.py
+++ grammotype/views.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
-@login_required
+#@login_required
 def home():
+    if not current_user.is_authenticated:
+        flash("Log in to save your results.", category='info')
     return render_template("home.html", user=current_user)
