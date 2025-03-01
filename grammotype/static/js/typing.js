0f89475451255ba07bb35789c834b2db55fa2739
andrewWong21 <awong541@gmail.com>
2025-02-28 23:58:36 -0500

    Adding test focusing and word blurring

--- grammotype/static/js/typing.js
+++ grammotype/static/js/typing.js
@@ -9,13 +9,17 @@ function randomWord(){
 }
 
 function formatWord(word){
-    return `<div class="word">${word} </div>`;
+    return `<div class="word">
+        <span class="letter">
+            ${word.split('').join('</span><span class="letter">')}
+        </span>
+    </div>`;
 }
 
 function newTest(){
     document.getElementById('words').innerHTML = '';
     for (let i = 0; i < 200; i++){
-        document.getElementById('words').innerHTML += randomWord();
+        document.getElementById('words').innerHTML += formatWord(randomWord());
     }
 }
 
